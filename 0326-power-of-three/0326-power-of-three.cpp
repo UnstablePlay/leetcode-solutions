4e8bb698743d15006b6df469c00cff299fc11d65
UnstablePlay <nair.anantha07@gmail.com>
2026-09-19 23:43:18 +0530

    Time: 0 ms (100%), Space: 9 MB (9.77%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <0) return false;
+        if (n == 1) return true;
+        if (n == 0) return false;        
+        while (n>0){
+            if (n%3 !=0 and n!=1) return false;                       
+            n = n/3;
+        }
+        return true;
+    }
+};
